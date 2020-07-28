e822702314290fa520f1f3e598817f3a2c0adf32
Rodrigo <rodrigoabbas@gmail.com>
2020-07-27 21:10:03 -0300

    Added custom  error handler middleware

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from '../routes';
-import generatePeopleArray from '../productsMaker';
+import { handleError } from '../src/utilities/errorHandler';
 
 const app = express();
-const router = express.Router();
+// const router = express.Router();
 
 // For parsing JSON data
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,21 +17,23 @@ app.get('/', (req, res) => {
   });
 });
 
-router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
+// router.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   next();
+// });
 
-routes(router);
-app.use('/api', router);
+// routes(router);
+app.use('/', routes);
 
-const port = process.env.PORT || 5000;
-
-generatePeopleArray();
+const port = process.env.PORT || 5050;
 
 app.use(express.static('dist'));
+
+app.use((err, req, res) => {
+  handleError(err, res);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
